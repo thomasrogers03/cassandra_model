e450e55241163adeccef29151e6abdf5c5d6fa5d
Thomas Rogers <ThomasRogers03@gmail.com>
2014-11-09 19:22:35 -0500

    Added configuration support to Record

--- lib/cassandra-model/record.rb
+++ lib/cassandra-model/record.rb
@@ -1,11 +1,26 @@
 class Record
+  DEFAULT_CONFIGURATION = {
+      'host' => 'localhost',
+      'keyspace' => 'default_keyspace',
+      'port' => '9042'
+  }
+
   class << self
+
     def table_name=(value)
       @table_name = value
     end
 
     def table_name
       @table_name ||= self.to_s.underscore.pluralize
+    end
+
+    def config=(value)
+      @@config = DEFAULT_CONFIGURATION.merge(value)
+    end
+
+    def config
+      @@config ||= DEFAULT_CONFIGURATION
     end
   end
 end
--- spec/classes/record_spec.rb
+++ spec/classes/record_spec.rb
@@ -19,4 +19,39 @@ describe Record do
       it { expect(Record.table_name).to eq('image_data') }
     end
   end
+
+  describe '.config' do
+    subject { Record.config }
+
+    let(:config) do
+      {
+          'host' => 'localhost',
+          'keyspace' => 'default_keyspace',
+          'port' => '9042'
+      }
+    end
+
+    it 'should use a default configuration' do
+      expect(subject).to eq(config)
+    end
+
+    context 'when specifying the options' do
+      let (:config) do
+        {
+            'host' => 'me',
+            'keyspace' => 'new_keyspace',
+            'port' => '9999'
+        }
+      end
+
+      before { Record.config = config  }
+
+      it { expect(subject).to eq(config) }
+    end
+
+    context 'when providing a configuration with missing keys' do
+      before { Record.config = {} }
+      it { expect(subject).to eq(config) }
+    end
+  end
 end
