7353c4c97db4cd2b310e5964b71d5ae7685a2e81
Thomas Rogers <ThomasRogers03@gmail.com>
2015-01-22 20:28:46 -0500

    Fixed example

--- examples/basic_queries.rb
+++ examples/basic_queries.rb
@@ -2,6 +2,7 @@ require_relative 'common'
 
 class ImageData < Record
   ImageData.primary_key = [:path, :data]
+  ImageData.columns = [:path, :data]
 end
 
 Record.config = { hosts: %w(me), keyspace: 'pcrawler' }
