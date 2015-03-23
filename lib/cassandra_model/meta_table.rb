8d8f8b4acc97742101018ccc2dee71fee16c2dc4
Thomas Rogers <ThomasRogers03@gmail.com>
2015-03-22 20:51:15 -0400

    Add the ability to create and drop the descriptor table for meta tables

--- lib/cassandra_model/meta_table.rb
+++ lib/cassandra_model/meta_table.rb
@@ -1,4 +1,25 @@
 module CassandraModel
   class MetaTable < Record
+
+    class << self
+      def create_descriptor_table
+        connection.execute(table_desc.to_cql)
+      end
+
+      def drop_descriptor_table
+        connection.execute("DROP TABLE #{table_name}")
+      end
+
+      private
+
+      def table_desc
+        @table_desc ||= begin
+          columns = {name: :ascii, created_at: :timestamp, id: :ascii}
+          options = {name: table_name, columns: columns, partition_key: [:name], clustering_columns: [:created_at, :id]}
+          TableDefinition.new(options)
+        end
+      end
+    end
+
   end
 end
--- spec/classes/meta_table_spec.rb
+++ spec/classes/meta_table_spec.rb
@@ -2,8 +2,28 @@ require 'rspec'
 
 module CassandraModel
   describe MetaTable do
+    let(:connection) { double(:connection) }
+
     subject { MetaTable.new({}) }
 
+    before { allow(MetaTable).to receive(:connection).and_return(connection) }
+
     it { is_expected.to be_a_kind_of(Record) }
+
+    describe '.create_descriptor_table' do
+      it 'should create the table in cassandra' do
+        expected_query = 'CREATE TABLE meta_tables (name ascii, created_at timestamp, id ascii, PRIMARY KEY ((name), created_at, id))'
+        expect(connection).to receive(:execute).with(expected_query)
+        MetaTable.create_descriptor_table
+      end
+    end
+
+    describe '.drop_descriptor_table' do
+      it 'should drop the table from cassandra' do
+        expected_query = 'DROP TABLE meta_tables'
+        expect(connection).to receive(:execute).with(expected_query)
+        MetaTable.drop_descriptor_table
+      end
+    end
   end
 end
