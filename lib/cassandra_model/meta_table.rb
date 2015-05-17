504f179d61a4ed92507eafa7911abe45585b356d
Thomas Rogers <ThomasRogers03@gmail.com>
2015-05-17 09:20:25 -0400

    Begin work on meta tables. No implementation yet...

---
+++ lib/cassandra_model/meta_table.rb
@@ -0,0 +1,5 @@
+module CassandraModel
+  class MetaTable < Table
+
+  end
+end
---
+++ spec/classes/meta_table_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+
+module CassandraModel
+  describe MetaTable do
+    let(:table_name) { :records }
+    let(:cluster) { double(:cluster, connect: connection) }
+    let(:connection) { double(:connection) }
+    let(:column_object) { double(:column, name: 'partition') }
+    let(:table_object) { double(:table, columns: [column_object]) }
+    let(:keyspace) { double(:keyspace, table: table_object) }
+    let(:klass) { MetaTable }
+
+    subject { klass.new(table_name) }
+
+    before do
+      klass.reset!
+      allow(Cassandra).to receive(:cluster).and_return(cluster)
+      allow(cluster).to receive(:keyspace).with(klass.config[:keyspace]).and_return(keyspace)
+    end
+
+    it_behaves_like 'a model with a connection', MetaTable
+    it_behaves_like 'a table'
+  end
+end
