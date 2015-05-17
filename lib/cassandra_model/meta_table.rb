176f144552ac33ddf92b96fd60159afd892b938d
Thomas Rogers <ThomasRogers03@gmail.com>
2015-05-17 12:51:38 -0400

    Dynamically create tables with MetaTable when requesting information about the table

--- lib/cassandra_model/meta_table.rb
+++ lib/cassandra_model/meta_table.rb
@@ -7,5 +7,15 @@ module CassandraModel
     def name
       @table_definition.name.to_s
     end
+
+    private
+
+    def table
+      @table ||= begin
+        descriptor = TableDescriptor.create(@table_definition)
+        connection.execute(@table_definition.to_cql) if descriptor.valid
+        keyspace.table(name)
+      end
+    end
   end
 end
--- spec/classes/meta_table_spec.rb
+++ spec/classes/meta_table_spec.rb
@@ -11,11 +11,20 @@ module CassandraModel
     end
     let(:definition) { TableDefinition.new(table_definition) }
     let(:cluster) { double(:cluster, connect: connection) }
-    let(:connection) { double(:connection) }
+    let(:connection) { double(:connection, execute: []) }
     let(:column_object) { double(:column, name: 'partition') }
-    let(:table_object) { double(:table, columns: [column_object]) }
+    let(:table_object) { double(:table, columns: [column_object], partition_key: [], clustering_columns: []) }
     let(:keyspace) { double(:keyspace, table: table_object) }
     let(:klass) { MetaTable }
+    let(:attributes) do
+      {name: definition.name.to_s,
+       created_at: Time.now,
+       id: definition.table_id}
+    end
+    let(:valid) { true }
+    let(:descriptor) do
+      TableDescriptor.new(attributes).tap { |desc| desc.invalidate! unless valid }
+    end
 
     subject { klass.new(definition) }
 
@@ -23,9 +32,34 @@ module CassandraModel
       klass.reset!
       allow(Cassandra).to receive(:cluster).and_return(cluster)
       allow(cluster).to receive(:keyspace).with(klass.config[:keyspace]).and_return(keyspace)
+      TableDescriptor.reset!
+      TableDescriptor.columns = [:name, :created_at, :id]
+      allow(TableDescriptor).to receive(:create).with(definition).and_return(descriptor)
     end
 
     it_behaves_like 'a model with a connection', MetaTable
     it_behaves_like 'a table'
+
+    [:partition_key, :clustering_columns, :columns].each do |method|
+      describe "#{method}" do
+
+        context 'when the table does not yet exist' do
+          it 'should create the table' do
+            expect(connection).to receive(:execute).with(definition.to_cql)
+            subject.public_send(method)
+          end
+        end
+
+        context 'when the table already exists' do
+          let(:valid) { false }
+
+          it 'should create the table' do
+            expect(connection).not_to receive(:execute).with(definition.to_cql)
+            subject.public_send(method)
+          end
+        end
+      end
+    end
+
   end
 end
