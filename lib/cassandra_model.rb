0f96d59bdf20353568cd898e78d6024c2a0a4bd3
Thomas Rogers <ThomasRogers03@gmail.com>
2015-06-03 21:02:13 -0400

    Add TableRedux to the include list

--- lib/cassandra_model.rb
+++ lib/cassandra_model.rb
@@ -16,6 +16,7 @@
 
 require 'cassandra_model/connection'
 require 'cassandra_model/table'
+require 'cassandra_model/table_redux'
 require 'cassandra_model/record'
 require 'cassandra_model/composite_record_static'
 require 'cassandra_model/composite_record_instance'
