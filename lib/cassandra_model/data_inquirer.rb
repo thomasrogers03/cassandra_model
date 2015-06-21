40de50be3824a110017732812dbc544498291b39
Thomas Rogers <ThomasRogers03@gmail.com>
2015-06-21 14:34:04 -0400

    Implement DataInquirer#defaults

--- lib/cassandra_model/data_inquirer.rb
+++ lib/cassandra_model/data_inquirer.rb
@@ -1,14 +1,31 @@
 module CassandraModel
   class DataInquirer
     attr_reader :partition_key
+    attr_reader :column_defaults
 
     def initialize
       @partition_key = Hash.new { |hash, key| hash[key] = :string }
+      @column_defaults = Hash.new { |hash, key| hash[key] = '' }
     end
 
     def knows_about(*columns)
-      columns.each { |column| partition_key[column] }
+      columns.each do |column|
+        partition_key[column]
+        column_defaults[column]
+      end
       self
+    end
+
+    def defaults(column)
+      ColumnDefault.new(column, column_defaults)
+    end
+
+    private
+
+    ColumnDefault = Struct.new(:column, :column_defaults) do
+      def to(value)
+        column_defaults[column] = value
+      end
     end
 
   end
--- spec/classes/data_inquirer_spec.rb
+++ spec/classes/data_inquirer_spec.rb
@@ -13,10 +13,34 @@ module CassandraModel
         expect(subject.partition_key).to eq(name: :string)
       end
 
+      it 'should define default values for the specified columns' do
+        subject.knows_about(:name)
+        expect(subject.column_defaults).to eq(name: '')
+      end
+
       context 'with different columns' do
         it 'should define the partition key' do
           subject.knows_about(:title, :series)
           expect(subject.partition_key).to eq(title: :string, series: :string)
+        end
+
+        it 'should define default values for the specified columns' do
+          subject.knows_about(:name)
+          expect(subject.column_defaults).to eq(name: '')
+        end
+      end
+    end
+
+    describe '#defaults' do
+      it 'should default the specified column to the requested value' do
+        subject.defaults(:title).to('NULL')
+        expect(subject.column_defaults).to eq(title: 'NULL')
+      end
+
+      context 'with a different column and default value' do
+        it 'should default the specified column to the requested value' do
+          subject.defaults(:series).to('1A')
+          expect(subject.column_defaults).to eq(series: '1A')
         end
       end
     end
