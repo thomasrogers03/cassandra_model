a97de074edb3f89232700f1d38f29a60e53322bf
Thomas Rogers <ThomasRogers03@gmail.com>
2015-06-22 20:53:25 -0400

    Implement DataSet#rotates_storage_across to help in defining table rotation for maintenance

--- lib/cassandra_model/data_set.rb
+++ lib/cassandra_model/data_set.rb
@@ -1,13 +1,18 @@
 module CassandraModel
   class DataSet
-    attr_reader :columns, :clustering_columns
+    attr_reader :columns, :clustering_columns, :data_rotation
 
     def initialize
       @columns = Hash.new { |hash, key| hash[key] = :text }
+      @data_rotation = {}
     end
 
     def knows_about(*columns)
       columns.each { |column| @columns[column] }
+    end
+
+    def rotates_storage_across(slices)
+      TableRotation.new(slices, self)
     end
 
     def is_defined_by(*columns)
@@ -22,9 +27,31 @@ module CassandraModel
 
     private
 
-    ColumnType = Struct.new(:column, :inquirer) do
+    ColumnType = Struct.new(:column, :data_set) do
       def to(type)
-        inquirer.columns[column] = type
+        data_set.columns[column] = type
+      end
+    end
+
+    TableRotation = Struct.new(:slices, :data_set) do
+      def tables
+        define_table_slicing
+        define_rotation_frequency(1.week)
+      end
+
+      def tables_every(interval)
+        define_table_slicing
+        define_rotation_frequency(interval)
+      end
+
+      private
+
+      def define_rotation_frequency(frequency)
+        data_set.data_rotation[:frequency] = frequency
+      end
+
+      def define_table_slicing
+        data_set.data_rotation[:slices] = slices
       end
     end
 
--- spec/classes/data_set_spec.rb
+++ spec/classes/data_set_spec.rb
@@ -25,6 +25,34 @@ module CassandraModel
       end
     end
 
+    describe '#rotates_storage_across' do
+      it 'should indicate that this data set will rotate the tables it is stored in' do
+        subject.rotates_storage_across(5).tables
+        expect(subject.data_rotation).to eq(slices: 5, frequency: 1.week)
+      end
+
+      context 'with a different number of table slices' do
+        it 'should use the specified number of tables to rotate' do
+          subject.rotates_storage_across(15).tables
+          expect(subject.data_rotation).to eq(slices: 15, frequency: 1.week)
+        end
+      end
+
+      context 'when specifying a frequency' do
+        it 'should the frequency to determine the rotation schedule' do
+          subject.rotates_storage_across(10).tables_every(20.minutes)
+          expect(subject.data_rotation).to eq(slices: 10, frequency: 20.minutes)
+        end
+
+        context 'with a different frequency' do
+          it 'should use the specified frequency' do
+            subject.rotates_storage_across(10).tables_every(5.hours)
+            expect(subject.data_rotation).to eq(slices: 10, frequency: 5.hours)
+          end
+        end
+      end
+    end
+
     describe '#is_defined_by' do
       it 'should define the clustering columns for this data set' do
         subject.is_defined_by(:name)
