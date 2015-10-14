30da6e37f24d68d508623aed46c5ad9fbc9d5161
Thomas Rogers <thomasrogers03@gmail.com>
2015-10-14 18:48:07 -0400

    Move shared spec for batching queries to the batching shared examples

--- spec/classes/record_spec.rb
+++ spec/classes/record_spec.rb
@@ -557,35 +557,6 @@ module CassandraModel
       end
     end
 
-    shared_examples_for 'a query running in a batch' do |method, args, statement_args|
-      let(:batch_type) { :logged }
-      let(:batch_klass) { SingleTokenLoggedBatch }
-      let(:batch) { double(:batch) }
-      let(:bound_statement) { double(:bound_statement) }
-
-      before do
-        allow(statement).to receive(:bind).with(*statement_args).and_return(bound_statement)
-        mock_reactor(cluster, batch_klass, {})
-        allow(global_reactor).to receive(:perform_within_batch).with(bound_statement).and_yield(batch).and_return(Cassandra::Future.value(['OK']))
-        Record.save_in_batch batch_type
-      end
-
-      it 'should add the record to the batch' do
-        expect(batch).to receive(:add).with(bound_statement)
-        Record.new(attributes).public_send(method, *args)
-      end
-
-      context 'with a different reactor type' do
-        let(:batch_type) { :unlogged }
-        let(:batch_klass) { SingleTokenUnloggedBatch }
-
-        it 'should add the record to the batch' do
-          expect(batch).to receive(:add).with(bound_statement)
-          Record.new(attributes).public_send(method, *args)
-        end
-      end
-    end
-
     describe '#save_async' do
       let(:table_name) { :table }
       let(:clustering_columns) { [] }
