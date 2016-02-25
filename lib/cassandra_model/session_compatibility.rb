81f58ffb56cc33f90991da30b07a587e5c09a2fd
Thomas Rogers <thomasrogers03@gmail.com>
2016-02-25 07:51:53 -0500

    Only install backwards compatibility hack when the driver version is >= 2

--- lib/cassandra_model/session_compatibility.rb
+++ lib/cassandra_model/session_compatibility.rb
@@ -1,16 +1,19 @@
-module Cassandra
-  class Session
+# only install this hack if our driver is incompatible with the old interface
+if Gem::Version.new(Gem.loaded_specs['cassandra-driver'].version) >= Gem::Version.new('2')
+  module Cassandra
+    class Session
 
-    alias :__execute_async :execute_async
+      alias :__execute_async :execute_async
 
-    def execute_async(statement, *args)
-      options = args.last.is_a?(Hash) ? args.pop : {}
-      __execute_async(statement, options.merge(arguments: args))
+      def execute_async(statement, *args)
+        options = args.last.is_a?(Hash) ? args.pop : {}
+        __execute_async(statement, options.merge(arguments: args))
+      end
+
+      def execute(statement, *args)
+        execute_async(statement, *args).get
+      end
+
     end
-
-    def execute(statement, *args)
-      execute_async(statement, *args).get
-    end
-
   end
 end
