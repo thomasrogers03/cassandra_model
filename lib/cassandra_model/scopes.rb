5185540ec45b1f0e91544bae94bb22ad2b2968ad
Thomas Rogers <thomasrogers03@gmail.com>
2016-03-23 18:23:08 -0400

    Change how we expose Record.scopes

--- lib/cassandra_model/scopes.rb
+++ lib/cassandra_model/scopes.rb
@@ -2,13 +2,13 @@ module CassandraModel
   module Scopes
     attr_reader :scopes
 
-    def self.extended(base)
-      base.instance_variable_set(:@scopes, {})
-    end
-
     def scope(name, callback)
       define_singleton_method(name, &callback)
       scopes[name] = callback
+    end
+
+    def scopes
+      @scopes ||= {}
     end
   end
 end
