463ab58f4bf99bb7b69149a7c3559e8e9dbead41
Thomas Rogers <thomasrogers03@gmail.com>
2016-05-04 18:38:45 -0400

    Refactor ResultPaginator#each_slice inner loop

--- lib/cassandra_model/result_paginator.rb
+++ lib/cassandra_model/result_paginator.rb
@@ -13,25 +13,36 @@ module CassandraModel
       each_slice { |slice| slice.each(&block) }
     end
 
-    def each_slice
+    def each_slice(&block)
       return to_enum(:each_slice) unless block_given?
 
       current_page = @page
-      loop do
+      while current_page
         page_results = current_page.get
         break if page_results.empty?
-        modified_results = page_results.map { |result| @callback.call(result, page_results.execution_info) }
-        if page_results.last_page?
-          yield modified_results
-          break
-        else
-          current_page = page_results.next_page_async
-          yield modified_results
-        end
+        modified_results = modified_page_results(page_results)
+        current_page = next_page(page_results, modified_results, &block)
       end
     end
 
     alias :get :to_a
+
+    private
+
+    def next_page(page_results, modified_results)
+      if page_results.last_page?
+        yield modified_results
+        nil
+      else
+        current_page = page_results.next_page_async
+        yield modified_results
+        current_page
+      end
+    end
+
+    def modified_page_results(page_results)
+      page_results.map { |result| @callback[result, page_results.execution_info] }
+    end
 
   end
 end
